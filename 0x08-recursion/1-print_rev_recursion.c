842e52b8a629dae91a106918d07a59a32782f8c8
Cymonz54 <cymonzchanua54@gmail.com>
2023-04-05 13:22:24 -0700

    0x08-recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,12 @@
 void _print_rev_recursion(char *s)
 
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_re_recursion(s + 1);
-		_putchar(*s);
+		return;
 	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s)
 }
